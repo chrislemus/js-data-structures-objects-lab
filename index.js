b8fc6ae96c28b0f9385320b20d0263a7151dc365
chrislemus <icrislemus@gmail.com>
2021-03-15 17:30:44 -0400

    commit tree 116b8c9b1533fd2abacb7182a9fb7d55d2db83f7

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+const driver = {}
+function updateDriverWithKeyAndValue(driver, key, val) {
+  return {...driver, [key]: val}
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value
+  return driver
+}
+
+function deleteFromDriverByKey(driver, key) {
+  return {...driver, [key]: undefined}
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key]
+  return driver
+}
